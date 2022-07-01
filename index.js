c92ac6bcb0f300e8a8afb4c2fd25255e0db074f7
Agung Prasetya <agungprasetya1121@gmail.com>
2022-07-01 15:02:25 +0700

    cloudinary

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-// import dotenv and call config function to load environment
-require("dotenv").config();
 const express = require("express");
-
-const cors = require("cors");
-
-// import this
-const http = require("http");
-const { Server } = require("socket.io");
-
-// Get routes to the variabel
-const router = require("./src/routes");
-
 const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: process.env.CLIENT_URL, // we must define cors because our client and server have diffe
-  },
-});
-
-// import socket function and call with parameter io
-require("./src/socket")(io);
-
+const router = require("./src/routes");
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use(cors());
-
-// Add endpoint grouping and router
-app.use("/api/v1/", router);
+app.use(express.json());
 app.use("/uploads", express.static("uploads"));
+
+app.use("/api/v1/", router);
 
 app.get("/", function (req, res) {
   res.send({
@@ -39,5 +19,4 @@ app.get("/", function (req, res) {
   });
 });
 
-// change app to server
-server.listen(port, () => console.log(`Listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
